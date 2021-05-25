d266b6f00c4ec2fef7ccb159e84d6a4516b5ca3a
hota1024 <hotalog@hota1024.com>
2021-05-26 03:00:13 +0900

    :sparkles: impl animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,40 @@ export const HomePage: NextPage = () => {
         <title>home - css-animation-portfolio</title>
       </Head>
 
-      <div>css-animation-portfolio</div>
+      <div className="container">
+        <div className="scene">
+          <div className="scene-0">
+            <div className="bg"></div>
+            <div className="bg"></div>
+            <div className="bg"></div>
+            <div className="bg">
+              <div className="content">
+                <h1>
+                  {'hello,world'.split('').map((c, key) => (
+                    <span key={key} className={`c${key + 1}`}>
+                      {c}
+                    </span>
+                  ))}
+                </h1>
+                <h1>
+                  {'にゃーん(=^・・^=)'.split('').map((c, key) => (
+                    <span key={key} className={`c${key + 1}`}>
+                      {c}
+                    </span>
+                  ))}
+                </h1>
+                <h1>
+                  {'I am hota1024!'.split('').map((c, key) => (
+                    <span key={key} className={`c${key + 1}`}>
+                      {c}
+                    </span>
+                  ))}
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
