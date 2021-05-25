d60adf38da61832a5492cb0a8c4aacb3eec93909
hota1024 <hotalog@hota1024.com>
2021-05-26 01:19:45 +0900

    :tada: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - css-animation-portfolio</title>
+      </Head>
+
+      <div>css-animation-portfolio</div>
+    </>
+  )
+}
+
+export default HomePage
